4b22b477438e1151522baad9d63bccafc6114d05
rioaldie <rioaldierwanto@gmail.com>
2024-09-06 22:35:21 +0700

    setup

---
+++ src/app/web.js
@@ -0,0 +1,15 @@
+import express from 'express';
+// import { publicRouter } from '../routes/public-api.js';
+// import { errorMiddleware } from '../middleware/error-middleware.js';
+// import { userRouter } from '../routes/user-api.js';
+
+export const web = express();
+
+web.use(express.json());
+
+// web.listen(port, () => {
+//   console.log('server running at http://localhost:' + port);
+// });
+// web.use(publicRouter);
+// web.use(userRouter);
+// web.use(errorMiddleware);
